fb91a4ec06087116e18a267910dde2b208986a9e
mrjoshuamartinez <mrjoshuamartinez@gmail.com>
2020-07-19 23:17:08 -0400

    fixed cube surface area formula

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMakerr {
+class CuboidMakers {
     constructor(dims) {
         this.length = dims.length;
         this.width = dims.width;
@@ -16,18 +16,15 @@ class CuboidMakerr {
     }
 }
 
-const cuboidd = new CuboidMakerr ({
+const cuboidd = new CuboidMakers ({
     length: 4,
     width: 5,
     height: 5
 });
 
-class CubeMaker extends CuboidMakerr {
+class CubeMaker extends CuboidMakers {
     constructor (dims) {
         super (dims);
-        this.length = dims.length;
-        this.width = dims.width;
-        this.height = dims.height;
     }
 
     volume () {
@@ -35,25 +32,25 @@ class CubeMaker extends CuboidMakerr {
     }
 
     surfaceArea () {
-        return (2 * (this.length * this.width + this.length * this.height + this.width * this.height));
+        return (6 * (this.length * this.length));
     }
 }
 
 const cube = new CubeMaker ({
     length: 4,
-    width: 5,
-    height: 5
+    width: 4,
+    height: 4,
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
-console.log("Total Volume from Class CuboidMakerr: ", cuboid.volume()); // 100
-console.log("Total Surface Area from Class CuboidMakerr: ", cuboid.surfaceArea()); // 130
+console.log("Total Volume from Class CuboidMakers: ", cuboid.volume()); // 100
+console.log("Total Surface Area from Class CuboidMakers: ", cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker. 
 // Find out the formulas for volume and surface area for cubes and create those methods using the
 // dimension properties from CuboidMaker. Test your work by logging out your volume and surface area.
 
-console.log("Total Volume from Class CubeMaker: ", cube.volume()); // 100
-console.log("Total Surface Area from Class CubeMaker: ", cuboid.surfaceArea()); // 130
+console.log("Total Volume from Class CubeMaker: ", cube.volume()); // 64
+console.log("Total Surface Area from Class CubeMaker: ", cube.surfaceArea()); // 96
